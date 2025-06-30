a237017cf3364289ef34d61caf151c96937b35cb
Lyla Winwolf <topalterluz.00.b@gmail.com>
2025-06-29 22:57:14 -0400

    apis

--- src/main/java/cl/duoc/MicroServicioRoles/entity/Rol.java
+++ src/main/java/cl/duoc/MicroServicioRoles/entity/Rol.java
@@ -1,5 +1,6 @@
 package cl.duoc.MicroServicioRoles.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -13,12 +14,15 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Entidad que representa un rol")
 public class Rol {
 
     @Id
     @Column(name = "ID_ROL")
+    @Schema(description = "id del rol")
     private int idRol;
 
     @Column(name = "NOMBRE_ROL", nullable = false, unique = true)
+    @Schema(description = "nombre del rol")
     private String nombreRol;
 }
